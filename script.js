5de131c42ab34bc6fc8a36f5a14925793427b719
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2023-09-12 19:34:32 +1200

    Create function that returns answer based on number and operator inputs

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const arr = [5, 10]; //for the purpose of testing the functions are working correctly
-let add = arr.reduce((total, current) => total + current,0);
 
-console.log(add);
-let subtract = arr.reduce((total, current) => total - current);
 
-console.log(subtract);
+function operate(num1, num2, operator) {
+    const arr = [num1, num2];
+    let add = arr.reduce((total, current) => total + current,0);
+    let subtract = arr.reduce((total, current) => total - current);
+    let multiply = arr.reduce((total, current) => total * current);
+    let divide = arr.reduce((total, current) => total / current);
+    if(operator === "+") {
+        return add;
+    } else if (operator === "-") {
+        return subtract;
+    } else if (operator === "*") {
+        return multiply;
+    } else{
+        return divide;
+    };
+};
 
-let multiply = arr.reduce((total, current) => total * current);
-
-console.log(multiply);
-
-let divide = arr.reduce((total, current) => total / current);
-
-console.log(divide);
+console.log("the function is " + operate(1, 5, "/"));
